4687eea770fb7c693da35cdeb5a1211fd198c50d
Alok Raj <alokr417@gmail.com>
2021-10-21 21:56:17 +0530

    custom tooltip text func added aka attribute enabled

--- tooltip.js
+++ tooltip.js
@@ -2,13 +2,18 @@ class Tooltip extends HTMLElement {
   constructor() {
     super(); // runs the constructor of the extended class i.e. HTMLElement in this class
     this._tooltipContainer;
+    this._tooltipText = 'Default tooltip text!'
   }
-
+  
   // Custom element's lifecycle:
   /*
-    Element Created -> constructor (Basic Initialization) -> Element attached to DOM -> connectedCallback (DOM Initializations) -> Element detached from DOM -> disconnectedCallback (CleanUp Work) -> (Observed Attribute update)adoptedCallback (Update Data + DOM) -> attributeChangedCallback ->
+  Element Created -> constructor (Basic Initialization) -> Element attached to DOM -> connectedCallback (DOM Initializations) -> Element detached from DOM -> disconnectedCallback (CleanUp Work) -> (Observed Attribute update)adoptedCallback (Update Data + DOM) -> attributeChangedCallback ->
   */
-  connectedCallback() {
+ connectedCallback() {
+   if(this.hasAttribute('text')) {
+      // Override default tooltip text with the attribute value 
+     this._tooltipText = this.getAttribute('text');
+   }
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
     tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
@@ -19,7 +24,7 @@ class Tooltip extends HTMLElement {
   // Underscore convention for Private Methods.
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = 'This is the tooltip text!';
+    this._tooltipContainer.textContent = this._tooltipText;
     this.appendChild(this._tooltipContainer);
   }
   _hideTooltip() {
