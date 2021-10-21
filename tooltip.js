6a35094ceafde9fb880626e87a4a3c8b36c57242
Alok Raj <alokr417@gmail.com>
2021-10-21 21:49:05 +0530

    Custom logic added for custom element

--- tooltip.js
+++ tooltip.js
@@ -1,6 +1,7 @@
 class Tooltip extends HTMLElement {
   constructor() {
     super(); // runs the constructor of the extended class i.e. HTMLElement in this class
+    this._tooltipContainer;
   }
 
   // Custom element's lifecycle:
@@ -10,7 +11,19 @@ class Tooltip extends HTMLElement {
   connectedCallback() {
     const tooltipIcon = document.createElement('span');
     tooltipIcon.textContent = ' (?)';
+    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
+    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
     this.appendChild(tooltipIcon);
+  }
+
+  // Underscore convention for Private Methods.
+  _showTooltip() {
+    this._tooltipContainer = document.createElement('div');
+    this._tooltipContainer.textContent = 'This is the tooltip text!';
+    this.appendChild(this._tooltipContainer);
+  }
+  _hideTooltip() {
+    this.removeChild(this._tooltipContainer);
   }
 }
 
