72c5b371e813fb431325dbfda7dcbdd184299e45
Alok Raj <alokr417@gmail.com>
2021-10-26 13:27:03 +0530

    attributeChangedCallback & disconnectedCallback

--- tooltip.js
+++ tooltip.js
@@ -1,68 +1,114 @@
 class Tooltip extends HTMLElement {
   constructor() {
-    super(); // runs the constructor of the extended class i.e. HTMLElement in this class
-    this._tooltipContainer;
-    this._tooltipText = 'Default tooltip text!'
+    super();
+    this._tooltipIcon;
+    this._tooltipVisible = false;
+    this._tooltipText = 'Some dummy tooltip text.';
     this.attachShadow({ mode: 'open' });
-    // Template part of the custom element
     this.shadowRoot.innerHTML = `
-      <style>
-        div {
-          background-color: #000;
-          color: #fff;
-          position: absolute;
-        }
-        :host(.important) {
-          background-color: #ccc;
-        }
-        :host-context(p.hello) {
-          font-weight: bold;
-        }
-        ::slotted(span) {
-          border: 1px solid blue;
-        }
-        .icon {
-          background: black;
-          color: white;
-          padding: 0.15rem 0.5rem;
-          text-align: center;
-          border-radius: 50%;
-        }
-      </style>
-      <slot>Default Slot Text</slot>
-      <span class="icon">?</span>
+        <style>
+            div {
+              font-weight: normal;
+              background-color: black;
+              color: white;
+              position: absolute;
+              top: 1.5rem;
+              left: 0.75rem;
+              z-index: 10;
+              padding: 0.15rem;
+              border-radius: 3px;
+              box-shadow: 1px 1px 6px rgba(0,0,0,0.26);
+            }
+
+            :host {
+              position: relative;
+            }
+
+            :host(.important) {
+              background: var(--color-primary, #ccc);
+              padding: 0.15rem;
+            }
+
+            :host-context(p) {
+              font-weight: bold;
+            }
+
+            .highlight {
+              background-color: red;
+            }
+
+            ::slotted(.highlight) {
+              border-bottom: 1px dotted red;
+            }
+
+            .icon {
+              background: black;
+              color: white;
+              padding: 0.15rem 0.5rem;
+              text-align: center;
+              border-radius: 50%;
+            }
+        </style>
+        <slot>Some default</slot>
+        <span class="icon">?</span>
     `;
   }
-  
-  // Custom element's lifecycle:
-  /*
-  Element Created -> constructor (Basic Initialization) -> Element attached to DOM -> connectedCallback (DOM Initializations) -> Element detached from DOM -> disconnectedCallback (CleanUp Work) -> (Observed Attribute update)adoptedCallback (Update Data + DOM) -> attributeChangedCallback ->
-  */
- // 💡 We will be using Shadow DOM to encapsulate our styles.
-//  If we are using shadowDOM then real DOM won't be disturbed.
- connectedCallback() {
-   if(this.hasAttribute('text')) {
-      // Override default tooltip text with the attribute value 
-     this._tooltipText = this.getAttribute('text');
-   }
-   const tooltipIcon = this.shadowRoot.querySelector('span');
-    tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
-    tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-    // Appending to shadow DOM so real DOM wont get affected
-    // with this, text wrapped inside custom element will get hidden
-    // So, now we will be using HTML template to show the text
-    this.shadowRoot.appendChild(tooltipIcon);
-    this.style.position = 'relative';
+
+  connectedCallback() {
+    if (this.hasAttribute('text')) {
+      this._tooltipText = this.getAttribute('text');
+    }
+    this._tooltipIcon = this.shadowRoot.querySelector('span');
+    this._tooltipIcon.addEventListener(
+      'mouseenter',
+      this._showTooltip.bind(this)
+    );
+    this._tooltipIcon.addEventListener(
+      'mouseleave',
+      this._hideTooltip.bind(this)
+    );
+    this._render();
   }
 
-  // Underscore convention for Private Methods.
-  _showTooltip() {
-    this._tooltipContainer = document.createElement('div');
-    this._tooltipContainer.textContent = this._tooltipText;
-    this.shadowRoot.appendChild(this._tooltipContainer);
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+    if (name === 'text') {
+      this._tooltipText = newValue;
+    }
   }
+
+  static get observedAttributes() {
+    return ['text'];
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener('mouseenter', this._showTooltip);
+    this._tooltipIcon.removeEventListener('mouseleave', this._hideTooltip);
+  }
+
+  _render() {
+    let tooltipContainer = this.shadowRoot.querySelector('div');
+    if (this._tooltipVisible) {
+      tooltipContainer = document.createElement('div');
+      tooltipContainer.textContent = this._tooltipText;
+      this.shadowRoot.appendChild(tooltipContainer);
+    } else {
+      if (tooltipContainer) {
+        this.shadowRoot.removeChild(tooltipContainer);
+      }
+    }
+  }
+
+  _showTooltip() {
+    this._tooltipVisible = true;
+    this._render();
+  }
+
   _hideTooltip() {
-    this.shadowRoot.removeChild(this._tooltipContainer);
+    this._tooltipVisible = false;
+    this._render();
   }
 }
 
