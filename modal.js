3aa3acdbb2797c033c06a96b2f43839211796145
Alok Raj <alokr417@gmail.com>
2021-10-27 09:52:06 +0530

    advance web component aka modal

--- modal.js
+++ modal.js
@@ -2,6 +2,7 @@ class Modal extends HTMLElement {
     constructor() {
       super();
       this.attachShadow({ mode: 'open' });
+      this.isOpen = false;
       this.shadowRoot.innerHTML = `
           <style>
               #backdrop {
@@ -15,9 +16,20 @@ class Modal extends HTMLElement {
                   opacity: 0;
                   pointer-events: none;
               }
+  
+              :host([opened]) #backdrop,
+              :host([opened]) #modal {
+                  opacity: 1;
+                  pointer-events: all;
+              }
+  
+              :host([opened]) #modal {
+                  top: 15vh;
+              }
+  
               #modal {
                   position: fixed;
-                  top: 15vh;
+                  top: 10vh;
                   left: 25%;
                   width: 50%;
                   z-index: 100;
@@ -29,14 +41,17 @@ class Modal extends HTMLElement {
                   justify-content: space-between;
                   opacity: 0;
                   pointer-events: none;
+                  transition: all 0.3s ease-out;
               }
   
               header {
                   padding: 1rem;
+                  border-bottom: 1px solid #ccc;
               }
   
-              header h1 {
+              ::slotted(h1) {
                   font-size: 1.25rem;
+                  margin: 0;
               }
   
               #main {
@@ -57,17 +72,70 @@ class Modal extends HTMLElement {
           <div id="backdrop"></div>
           <div id="modal">
               <header>
-                  <h1>Please Confirm</h1>
+                  <slot name="title">Please Confirm Payment</slot>
               </header>
               <section id="main">
                   <slot></slot>
               </section>
               <section id="actions">
-                  <button>Cancel</button>
-                  <button>Okay</button>
+                  <button id="cancel-btn">Cancel</button>
+                  <button id="confirm-btn">Okay</button>
               </section>
           </div>
       `;
+      const slots = this.shadowRoot.querySelectorAll('slot');
+      slots[1].addEventListener('slotchange', event => {
+        console.dir(slots[1].assignedNodes());
+      });
+      const backdrop = this.shadowRoot.querySelector('#backdrop');
+      const cancelButton = this.shadowRoot.querySelector('#cancel-btn');
+      const confirmButton = this.shadowRoot.querySelector('#confirm-btn');
+      backdrop.addEventListener('click', this._cancel.bind(this));
+      cancelButton.addEventListener('click', this._cancel.bind(this));
+      confirmButton.addEventListener('click', this._confirm.bind(this));
+      // cancelButton.addEventListener('cancel', () => {
+      //   console.log('Cancel inside the component');
+      // });
+    }
+  
+    attributeChangedCallback(name, oldValue, newValue) {
+      if (this.hasAttribute('opened')) {
+        this.isOpen = true;
+        // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+        // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+        // this.shadowRoot.querySelector('#modal').style.opacity = 1;
+        // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+      } else {
+        this.isOpen = false;
+      }
+    }
+  
+    static get observedAttributes() {
+      return ['opened'];
+    }
+  
+    open() {
+      this.setAttribute('opened', '');
+      this.isOpen = true;
+    }
+  
+    hide() {
+      if (this.hasAttribute('opened')) {
+        this.removeAttribute('opened');
+      }
+      this.isOpen = false;
+    }
+  
+    _cancel(event) {
+      this.hide();
+      const cancelEvent = new Event('cancel', { bubbles: true, composed: true });
+      event.target.dispatchEvent(cancelEvent);
+    }
+  
+    _confirm() {
+      this.hide();
+      const confirmEvent = new Event('confirm');
+      this.dispatchEvent(confirmEvent);
     }
   }
   
